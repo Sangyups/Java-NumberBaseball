723879b8c686506bd8f37d571c11440f1766fcc8
Sangyups <feels4637@gmail.com>
2021-11-23 01:47:57 +0900

    inital setting

---
+++ src/baseball/Application.java
@@ -0,0 +1,10 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        // 여기에 작성.
+    }
+}
