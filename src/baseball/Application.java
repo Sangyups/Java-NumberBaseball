a94eadb6b255bd3b08b82e29393535663faaaac2
Sangyups <feels4637@gmail.com>
2021-11-24 00:53:24 +0900

    refactor: separate handling error input and starting game

--- src/baseball/Application.java
+++ src/baseball/Application.java
@@ -6,7 +6,18 @@ public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         Game game = new Game();
-        String inputNumber = scanner.next();
-        game.guessNumber(inputNumber);
+        boolean isError;
+        int number = 0;
+        do {
+            try {
+                isError = false;
+                String inputNumber = scanner.next();
+                number = game.checkInput(inputNumber);
+            } catch (Exception e) {
+                System.err.println("적절한 숫자를 입력해주세요.");
+                isError = true;
+            }
+        } while (isError);
+        game.guessNumber(number);
     }
 }
--- src/baseball/Game.java
+++ src/baseball/Game.java
@@ -25,56 +25,56 @@ public class Game {
         randomNumber.forEach(System.out::println);
     }
 
-    private int checkInput(String inputNumber) {
+    public int checkInput(String inputNumber) {
         int number = -1;
         try {
 //            사용자가 숫자를 입력했는가?
             number = Integer.parseInt(inputNumber);
         } catch (Exception e) {
-            System.err.println("Invalid Input");
             return number;
         }
 //        숫자가 size만큼의 자리수가 아닐때
-        if(number / Math.pow(10, size-1) > 9 || number / Math.pow(10, size-1) < 1 ) return -1;
+        if (number / Math.pow(10, size - 1) > 9 || number / Math.pow(10, size - 1) < 1) {
+            throw new IllegalArgumentException();
+        }
         return number;
     }
+
     private ArrayList<Integer> sliceInput(int inputNumber) {
         ArrayList<Integer> slicedInput = new ArrayList<>();
-        for(int i = size -1; i >= 0; i--) {
-            int div = (int)Math.pow(10, i);
-            slicedInput.add(size-1-i, inputNumber / div);
+        for (int i = size - 1; i >= 0; i--) {
+            int div = (int) Math.pow(10, i);
+            slicedInput.add(size - 1 - i, inputNumber / div);
             inputNumber %= div;
         }
         return slicedInput;
     }
-    public void guessNumber(String inputNumber) {
-        int gotNumber = checkInput(inputNumber);
+
+    public void guessNumber(int inputNumber) {
+
         ArrayList<Integer> slicedInput = new ArrayList<>();
-        if(gotNumber == -1){
-            throw new IllegalArgumentException();
-        } else {
-            slicedInput = sliceInput(gotNumber);
-        }
+
+        slicedInput = sliceInput(inputNumber);
+
 //        current index, strikes, balls
         int[] scores = {0, 0, 0};
 
-        slicedInput.forEach(i->{
-            if(randomNumber.indexOf(i) == scores[0]++) scores[1]++;
-            else if(randomNumber.indexOf(i) != -1) scores[2]++;
+        slicedInput.forEach(i -> {
+            if (randomNumber.indexOf(i) == scores[0]++) scores[1]++;
+            else if (randomNumber.indexOf(i) != -1) scores[2]++;
         });
-        String result="";
-
-        if(scores[2] > 0) {
+        String result = "";
+        if (scores[2] > 0) {
             result += scores[2] + "볼 ";
         }
-        if(scores[1] > 0) {
+        if (scores[1] > 0) {
             result += scores[1] + "스트라이크";
         }
-        if(scores[1] == 0 && scores[2] == 0) {
-            result="낫싱";
+        if (scores[1] == 0 && scores[2] == 0) {
+            result = "낫싱";
         }
         System.out.println(result);
-        if(scores[1] == size) {
+        if (scores[1] == size) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         }
         return;
