c8ea26522e414684261c71125d82c8f87f05e964
Sangyups <feels4637@gmail.com>
2021-11-24 02:11:36 +0900

    feat: add whether to restart game or not

--- src/baseball/Application.java
+++ src/baseball/Application.java
@@ -7,10 +7,12 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        Game game = new Game();
+        do {
+            Game game = new Game();
 
-        int size = game.getSize();
-        int number = InputHandler.userGuessInteraction(size);
-        game.startGame(number);
+            int size = game.getSize();
+            int number = InputHandler.userGuessInteraction(size);
+            game.startGame(number);
+        } while (InputHandler.userRestartInteraction() == 1);
     }
 }
--- src/utils/InputHandler.java
+++ src/utils/InputHandler.java
@@ -25,14 +25,37 @@ public class InputHandler {
         return number;
     }
 
+    public static int inputRestartHandler(String inputNumber) {
+        int number = -1;
+        try {
+//            사용자가 숫자를 입력했는가
+            number = Integer.parseInt(inputNumber);
+            if (number != 1 && number != 2) throw new IllegalArgumentException();
+        } catch (Exception e) {
+            System.err.println("맞는 값을 입력해주세요");
+            return -1;
+        }
+        return number;
+    }
+
     public static int userGuessInteraction(int size) {
         final Scanner scanner = new Scanner(System.in);
         int number = 0;
         do {
             System.out.print("숫자를 입력해주세요 : ");
             String inputNumber = scanner.next();
-            number = InputHandler.inputNumberHandler(inputNumber, size);
+            number = inputNumberHandler(inputNumber, size);
         } while (number == -1);
         return number;
+    }
+
+    public static int userRestartInteraction() {
+        final Scanner scanner = new Scanner(System.in);
+        int restart = 0;
+        do {
+            String inputNumber = scanner.next();
+            restart = inputRestartHandler(inputNumber);
+        } while (restart == -1);
+        return restart;
     }
 }
