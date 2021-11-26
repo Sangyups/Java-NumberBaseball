06695cd584a3811f3ced523bc27ad9f77aa2c8d4
Sangyups <feels4637@gmail.com>
2021-11-26 20:15:20 +0900

    refactor: organize logic of getting guess number from user

--- src/utils/InputUtils.java
+++ src/utils/InputUtils.java
@@ -4,25 +4,33 @@ import java.util.Scanner;
 
 public class InputUtils {
 
-    Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
     private InputUtils() {
     }
 
-    private static void checkDigit(int number, int size) {
-        if (number / Math.pow(10, size - 1) >= 10 || number / Math.pow(10, size - 1) < 1) {
-            throw new IllegalArgumentException();
-        }
+    private static void checkDigit(String number, int size) {
+        if (number.length() != 3) throw new IllegalArgumentException();
     }
 
-    public static int inputNumberHandler(String inputNumber, int size) {
+    private static int getNumberOrException(String input) {
+        int parsedInput;
+        try {
+            parsedInput = Integer.parseInt(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+        return parsedInput;
+    }
+
+    public static int getNumberIfValid(String inputNumber, int size) {
         int number = -1;
         try {
-//            사용자가 숫자를 입력했는가
-            number = Integer.parseInt(inputNumber);
 //            자릿수가 일치하는가
-            checkDigit(number, size);
-        } catch (Exception e) {
+            checkDigit(inputNumber, size);
+//            사용자가 숫자를 입력했는가
+            number = getNumberOrException(inputNumber);
+        } catch (IllegalArgumentException e) {
             return -1;
         }
         return number;
@@ -42,12 +50,11 @@ public class InputUtils {
     }
 
     public static int userGuessInteraction(int size) {
-        final Scanner scanner = new Scanner(System.in);
-        int number = 0;
+        int number = -1;
         do {
             System.out.print("숫자를 입력해주세요 : ");
-            String inputNumber = scanner.next();
-            number = inputNumberHandler(inputNumber, size);
+            String inputNumber = scanner.nextLine();
+            number = getNumberIfValid(inputNumber, size);
         } while (number == -1);
         return number;
     }
